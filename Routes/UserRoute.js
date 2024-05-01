7896e9dd7c0b0faa2ea7a14fd2a6621a8cf47a98
Abdul Ragib <abdulragib36@gmail.com>
2024-05-01 08:49:09 +0530

    getalluser controller

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -3,13 +3,14 @@ import {
   UnfollowUser,
   deleteUser,
   followUser,
+  getAllUsers,
   getUser,
   updateUser,
 } from "../Controllers/UserController.js";
 const router = express.Router();
 
+router.get('/',getAllUsers)
 router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
-
 router.put("/:id/follow", followUser);
 router.put("/:id/unfollow", UnfollowUser);
 
